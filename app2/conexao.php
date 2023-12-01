96d68d5d122a2a1419e1ab875f95353d51cd1871
Helter Pitanga <helterpitanga@gmail.com>
2023-12-01 13:01:07 -0300

    Novo App2

--- app2/conexao.php
+++ app2/conexao.php
@@ -1,7 +1,8 @@
 <?php
-$servidor = "localhost";
-$usuario = "";
-$senha = "";
-$dbname = "";
+$servidor = "mysql";
+$usuario = "root";
+$senha = "senha123";
+$dbname = "meubanco";
+
 
 $conn = mysqli_connect($servidor,$usuario,$senha,$dbname)
