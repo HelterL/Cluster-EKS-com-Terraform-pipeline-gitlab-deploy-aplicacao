966173baae8cc0154b897457672ab43ea9798eb7
Helter Pitanga <helterpitanga@gmail.com>
2023-12-01 17:15:57 -0300

    Mudando arquivos PHP

--- app2/processa.php
+++ app2/processa.php
@@ -1,8 +1,6 @@
-<?php include "conexao.php"?>
-
 <?php
-
 session_start();
+include_once("conexao.php");
 
 $nome = filter_input(INPUT_POST,'nome', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST,'email', FILTER_SANITIZE_EMAIL);
